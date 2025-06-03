3dee2e32939e324dfd86e6ec0dbc4f23dde12bd5
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-06-03 14:26:31 -0400

    Add gameboard class add function to placeship add function to fire shot add ability to track shots add ability to check if ships have sunk

--- src/constructors.js
+++ src/constructors.js
@@ -17,4 +17,98 @@ class Ship {
   };
 }
 
-module.exports = { Ship };
+class Gameboard {
+  constructor() {
+    this.carrier = [new Ship(5), ,];
+    this.battleship = [new Ship(4), ,];
+    this.destroyer = [new Ship(3), ,];
+    this.submarine = [new Ship(3), ,];
+    this.patrolboat = [new Ship(2), ,];
+    this.shipyard = [];
+    this.shots = [];
+  }
+
+  placeShip = function (type, x, y) {
+    let tmpx = x.split("");
+    let tmpy = y.split("");
+    if (tmpx[0] !== tmpy[0] && tmpx[1] !== tmpy[1]) {
+      return;
+    }
+    x = tmpx[0].charCodeAt(0) * 10 + (tmpx[1] - 1);
+    y = tmpy[0].charCodeAt(0) * 10 + (tmpy[1] - 1);
+    if (x > y) {
+      let tmp = y;
+      y = x;
+      x = tmp;
+    }
+    if (type == "carrier") {
+      this.carrier[1] = x;
+      this.carrier[2] = y;
+    }
+    if (type == "battleship") {
+      this.battleship[1] = x;
+      this.battleship[2] = y;
+    }
+    if (type == "destroyer") {
+      this.destroyer[1] = x;
+      this.destroyer[2] = y;
+    }
+    if (type == "submarine") {
+      this.submarine[1] = x;
+      this.submarine[2] = y;
+    }
+    if (type == "patrolboat") {
+      this.patrolboat[1] = x;
+      this.patrolboat[2] = y;
+    }
+    if (tmpx[0] == tmpy[0]) {
+      for (let z = 0; z < y - x + 1; z++) {
+        this.shipyard.push([x + z, type]);
+      }
+    } else {
+      for (let z = 0; z < Math.floor((y - x) / 10) + 1; z++) {
+        this.shipyard.push([x + z * 10, type]);
+      }
+    }
+  };
+
+  receiveAttack = function (x) {
+    let tmpx = x.split("");
+    x = tmpx[0].charCodeAt(0) * 10 + (tmpx[1] - 1);
+    this.shots.push(x);
+    for (let z = 0; z < this.shipyard.length; z++) {
+      if (this.shipyard[z][0] == x) {
+        if (this.shipyard[z][1] == "carrier") {
+          this.carrier[0].hit();
+        } else if (this.shipyard[z][1] == "battleship") {
+          this.battleship[0].hit();
+        } else if (this.shipyard[z][1] == "destroyer") {
+          this.destroyer[0].hit();
+        } else if (this.shipyard[z][1] == "submarine") {
+          this.submarine[0].hit();
+        } else {
+          this.patrolboat[0].hit();
+        }
+        allSunk();
+        return "hit";
+      } else {
+        return "miss";
+      }
+    }
+  };
+
+  allSunk = function () {
+    if (
+      this.carrier[0].sunk == "yes" &&
+      this.battleship[0].sunk == "yes" &&
+      this.destroyer[0].sunk == "yes" &&
+      this.submarine[0].sunk == "yes" &&
+      this.patrolboat[0].sunk == "yes"
+    ) {
+      return "All sank";
+    }
+    return "Some left";
+  };
+}
+
+module.exports = { Ship, Gameboard };
--- src/jest.test.js
+++ src/jest.test.js
@@ -1,4 +1,4 @@
-const { Ship } = require("./constructors.js");
+const { Ship, Gameboard } = require("./constructors.js");
 
 test(`newship`, () => {
   const testship = new Ship(3);
@@ -19,4 +19,52 @@ test(`hitship`, () => {
   testship.hit();
   testship.hit();
   expect(testship.sunk).toBe("yes");
+});
+
+test(`placeship`, () => {
+  const board = new Gameboard();
+  board.placeShip("carrier", "a1", "a5");
+  expect(board.carrier[1]).toBe(970);
+  expect(board.carrier[2]).toBe(974);
+});
+
+test(`placeship`, () => {
+  const board = new Gameboard();
+  board.placeShip("carrier", "a5", "a1");
+  expect(board.carrier[1]).toBe(970);
+  expect(board.carrier[2]).toBe(974);
+  expect(board.shipyard[0][0]).toBe(970);
+  expect(board.shipyard[4][0]).toBe(974);
+});
+
+test(`placeship`, () => {
+  const board = new Gameboard();
+  board.placeShip("carrier", "a2", "e2");
+  expect(board.carrier[1]).toBe(971);
+  expect(board.carrier[2]).toBe(1011);
+  expect(board.shipyard[0][0]).toBe(971);
+  expect(board.shipyard[4][0]).toBe(1011);
+});
+
+test(`shipsize`, () => {
+  const board = new Gameboard();
+  expect(board.carrier[0].length).toBe(5);
+  expect(board.battleship[0].length).toBe(4);
+  expect(board.destroyer[0].length).toBe(3);
+  expect(board.patrolboat[0].length).toBe(2);
+});
+
+test(`attackship`, () => {
+  const board = new Gameboard();
+  board.placeShip("carrier", "a5", "a1");
+  board.receiveAttack("a1");
+  expect(board.carrier[0].hits).toBe(1);
+});
+
+test(`attackship`, () => {
+  const board = new Gameboard();
+  board.placeShip("carrier", "a5", "a1");
+  board.receiveAttack("a1");
+  expect(board.shots[0]).toBe(970);
+  expect(board.receiveAttack("b1")).toBe("miss");
 });
